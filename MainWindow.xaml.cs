ecd3d245dac616925a40c04aa2ec91c2bd90d1cb
Munteanu Teofil <andreiteofil01@gmail.com>
2022-12-04 18:25:19 +0200

    Adding Players

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,10 +26,10 @@ namespace Deez_Notes_Dm
             try
             {
 
-                string json = System.IO.File.ReadAllText(@"Resources/Players/Test.json");
+                string json = System.IO.File.ReadAllText(@"Resources/Players/Players.json");
 
 
-                PlayerList.ItemsSource = JsonConvert.DeserializeObject<List<Test>>(json);
+                PlayerList.ItemsSource = JsonConvert.DeserializeObject<List<Player>>(json);
             }
             catch (Exception ex)
             {
@@ -39,7 +39,46 @@ namespace Deez_Notes_Dm
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            this.NewPlayerForm.Visibility = Visibility.Visible;
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String Name = this.NameInput.Text;
+                String Race = this.RaceInput.Text;
+                int HP = Int32.Parse(this.ACInput.Text);
+                int AC = Int32.Parse(this.ACInput.Text);
+                Dictionary<String, int> Stats = new Dictionary<String, int>
+                {
+                    {"STR", Int32.Parse(this.STRInput.Text)},
+                    {"DEX", Int32.Parse(this.DEXInput.Text)},
+                    {"CON", Int32.Parse(this.CONInput.Text)},
+                    {"INT", Int32.Parse(this.INTInput.Text)},
+                    {"WIS", Int32.Parse(this.WISInput.Text)},
+                    {"CHA", Int32.Parse(this.CHAInput.Text)}
+                };
+
+                Player player = new Player(Name, Race, HP, AC, Stats);
+
+                string json = System.IO.File.ReadAllText(@"Resources/Players/Players.json");
+                string newJson;
+
+                List<Player> players = JsonConvert.DeserializeObject<List<Player>>(json);
+                players.Add(player);
+                newJson = JsonConvert.SerializeObject(players);
+
+                System.IO.File.WriteAllText(@"Resources/Players/Players.json", newJson);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            OutputJson();
+
+            this.NewPlayerForm.Visibility = Visibility.Collapsed;
         }
     }
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -3,74 +3,109 @@ using System.Collections.Generic;
 
 namespace Deez_Notes_Dm.Models
 {
-    internal class Player
+    public class Players
     {
-        String name;
-        String race;
+        public List<Player> players { get; set; }
+    }
+    public class Player
+    {
+        public String Name { get; set; }
+        public String Race { get; set; }
 
-        int XP;
-        int[] XPbyLevel = { -1, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000 };
-        int totalLevel;
-        Dictionary<string, int> levelByClass = new Dictionary<string, int>();
-
-
-        int AC;
-        int maxHP;
-        int HP;
-        int HitDice;
-
-        int succededDeathSaves;
-        int failedDeathSaves;
+        public int XP { get; set; }
+        public int[] XPbyLevel { get; set; } = { -1, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000 };
+        public int totalLevel { get; set; }
+        public Dictionary<string, int> levelByClass { get; set; } = new Dictionary<string, int>();
 
 
-        Dictionary<string, int> stats = new Dictionary<string, int> {
-            {"STR", 0},
-            {"DEX", 0},
-            {"CON", 0},
-            {"INT", 0},
-            {"WIS", 0},
-            {"CHA", 0}
-        };
+        public int AC { get; set; }
+        public int maxHP { get; set; }
+        public int HP { get; set; }
+        public int HitDice { get; set; }
 
-        int passiveInsight;
-        int passiveInvestigation;
-        int passivePerception;
+        public int succededDeathSaves { get; set; }
+        public int failedDeathSaves { get; set; }
 
-        int[] proficiencyByLevel = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
-        bool proficiencyInsight;
-        bool proficiencyInvestigation;
-        bool proficiencyPerception;
+        private Dictionary<String, int> _Stats;
+
+        public Dictionary<String, int> Stats
+        {
+            get { return _Stats; }
+            set { _Stats = value; }
+        }
+
+
+
+        //public int passiveInsight { get; set; }
+        //public int passiveInvestigation { get; set; }
+        //public int passivePerception { get; set; }
+
+        //public int[] proficiencyByLevel { get; set; } = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
+        //public bool proficiencyInsight { get; set; }
+        //public bool proficiencyInvestigation { get; set; }
+        //public bool proficiencyPerception { get; set; }
+
+
+        //public int initiative { get; set; }
 
 
         int getModifier(String statBlock)
         {
-            return (int)Math.Floor(((stats[statBlock] - 10)) / 2.0);
+            return (int)Math.Floor(((Stats[statBlock] - 10)) / 2.0);
         }
 
-
-        Player(String name, String race, String _class, int HP, int AC, Dictionary<string, int> stats,
-            bool proficiencyInsight, bool proficiencyInvestigation, bool proficiencyPerception)
+        public Player()
         {
-            this.name = name;
-            this.race = race;
+
+        }
+
+        public Player(String name, String race, int HP, int AC, Dictionary<string, int> stats)
+        {
+            Stats = new Dictionary<String, int> {
+                {"STR", 0},
+                {"DEX", 0},
+                {"CON", 0},
+                {"INT", 0},
+                {"WIS", 0},
+                {"CHA", 0}
+            };
+
+            this.Name = name;
+            this.Race = race;
 
             this.XP = 0;
             this.totalLevel = 1;
-            this.levelByClass.Add(_class, 1);
 
             this.AC = AC;
             this.maxHP = this.HP = HP;
             this.HitDice = 1;
 
-            this.stats = stats;
-            this.proficiencyInsight = proficiencyInsight;
-            this.proficiencyInvestigation = proficiencyInvestigation;
-            this.proficiencyPerception = proficiencyPerception;
-
-            Console.WriteLine(getModifier("WIS"));
-            this.passiveInsight = 10 + getModifier("WIS") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
-            this.passiveInvestigation = 10 + getModifier("INT") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
-            this.passivePerception = 10 + getModifier("WIS") + (proficiencyPerception ? proficiencyByLevel[1] : 0);
+            this.Stats = stats;
         }
+
+        //public Player(String name, String race, String _class, int HP, int AC, Dictionary<string, int> stats,
+        //    bool proficiencyInsight, bool proficiencyInvestigation, bool proficiencyPerception)
+        //{
+        //    this.Name = name;
+        //    this.Race = race;
+
+        //    this.XP = 0;
+        //    this.totalLevel = 1;
+        //    this.levelByClass.Add(_class, 1);
+
+        //    this.AC = AC;
+        //    this.maxHP = this.HP = HP;
+        //    this.HitDice = 1;
+
+        //    this.stats = stats;
+        //    this.proficiencyInsight = proficiencyInsight;
+        //    this.proficiencyInvestigation = proficiencyInvestigation;
+        //    this.proficiencyPerception = proficiencyPerception;
+
+        //    Console.WriteLine(getModifier("WIS"));
+        //    this.passiveInsight = 10 + getModifier("WIS") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
+        //    this.passiveInvestigation = 10 + getModifier("INT") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
+        //    this.passivePerception = 10 + getModifier("WIS") + (proficiencyPerception ? proficiencyByLevel[1] : 0);
+        //}
     }
 }
