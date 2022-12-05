c7b75ad61b26864d4e64b3e6d1a05340f24d8e99
Munteanu Teofil <andreiteofil01@gmail.com>
2022-12-05 11:48:19 +0200

    Update Player.cs

--- Models/Player.cs
+++ Models/Player.cs
@@ -12,8 +12,8 @@ namespace Deez_Notes_Dm.Models
         public String Name { get; set; }
         public String Race { get; set; }
 
-        public int XP { get; set; }
         static int[] XPbyLevel { get; set; } = { -1, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000 };
+        public int XP { get; set; }
         public int totalLevel { get; set; }
         public Dictionary<string, int> levelByClass { get; set; } = new Dictionary<string, int>();
 
@@ -57,17 +57,14 @@ namespace Deez_Notes_Dm.Models
         }
         public StatsModifiers StatsMod { get; set; }
 
-        //public int passiveInsight { get; set; }
-        //public int passiveInvestigation { get; set; }
-        //public int passivePerception { get; set; }
+        public int passiveInsight { get; set; }
+        public int passiveInvestigation { get; set; }
+        public int passivePerception { get; set; }
 
-        //public int[] proficiencyByLevel { get; set; } = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
+        static int[] proficiencyByLevel { get; set; } = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
         //public bool proficiencyInsight { get; set; }
         //public bool proficiencyInvestigation { get; set; }
         //public bool proficiencyPerception { get; set; }
-
-
-        //public int initiative { get; set; }
 
 
         int getModifier(int statusValue)
@@ -117,29 +114,35 @@ namespace Deez_Notes_Dm.Models
             
         }
 
-        //public Player(String name, String race, String _class, int HP, int AC, Dictionary<string, int> stats,
-        //    bool proficiencyInsight, bool proficiencyInvestigation, bool proficiencyPerception)
-        //{
-        //    this.Name = name;
-        //    this.Race = race;
+        public Player(String name, String race, String _class, int HP, int AC, Status stats,
+            bool proficiencyInsight, bool proficiencyInvestigation, bool proficiencyPerception)
+        {
+            this.Name = name;
+            this.Race = race;
 
-        //    this.XP = 0;
-        //    this.totalLevel = 1;
-        //    this.levelByClass.Add(_class, 1);
+            this.XP = 0;
+            this.totalLevel = 1;
 
-        //    this.AC = AC;
-        //    this.maxHP = this.HP = HP;
-        //    this.HitDice = 1;
+            this.AC = AC;
+            this.maxHP = this.HP = HP;
+            this.HitDice = 1;
 
-        //    this.stats = stats;
-        //    this.proficiencyInsight = proficiencyInsight;
-        //    this.proficiencyInvestigation = proficiencyInvestigation;
-        //    this.proficiencyPerception = proficiencyPerception;
+            this.Stats = stats;
 
-        //    Console.WriteLine(getModifier("WIS"));
-        //    this.passiveInsight = 10 + getModifier("WIS") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
-        //    this.passiveInvestigation = 10 + getModifier("INT") + (proficiencyInsight ? proficiencyByLevel[1] : 0);
-        //    this.passivePerception = 10 + getModifier("WIS") + (proficiencyPerception ? proficiencyByLevel[1] : 0);
-        //}
+            this.StatsMod = new StatsModifiers();
+            StatsMod.STR = getModifier(Stats.STR);
+            StatsMod.DEX = getModifier(Stats.DEX);
+            StatsMod.CON = getModifier(Stats.CON);
+            StatsMod.INT = getModifier(Stats.INT);
+            StatsMod.WIS = getModifier(Stats.WIS);
+            StatsMod.CHA = getModifier(Stats.CHA);
+
+            //this.proficiencyInsight = proficiencyInsight;
+            //this.proficiencyInvestigation = proficiencyInvestigation;
+            //this.proficiencyPerception = proficiencyPerception;
+            this.passiveInsight = 10 + StatsMod.WIS + (proficiencyInsight ? proficiencyByLevel[1] : 0);
+            this.passiveInvestigation = 10 + StatsMod.INT + (proficiencyInvestigation ? proficiencyByLevel[1] : 0);
+            this.passivePerception = 10 + StatsMod.WIS + (proficiencyPerception ? proficiencyByLevel[1] : 0);
+        }
     }
 }
