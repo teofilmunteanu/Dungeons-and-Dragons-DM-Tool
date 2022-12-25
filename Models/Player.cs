f17fb8730e0ea86fc5ea56755116364018f379b8
Munteanu Teofil <andreiteofil01@gmail.com>
2022-12-25 03:01:19 +0200

    Update Player.cs

--- Models/Player.cs
+++ Models/Player.cs
@@ -24,11 +24,14 @@ namespace Deez_Notes_Dm.Models
 
 
         public int PassiveInsight { get; set; }
-        public int PassiveInvestigation { get; set; }
         public int PassivePerception { get; set; }
+        public int PassiveInvestigation { get; set; }
 
         private static readonly int[] proficiencyByLevel = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
 
+        private bool proficiencyInsight;
+        private bool proficiencyInvestigation;
+        private bool proficiencyPerception;
 
         public Player()
         {
@@ -38,37 +41,6 @@ namespace Deez_Notes_Dm.Models
         {
             PlayerCount--;
         }
-
-        //public Player(string name, string race, string _class, int HP, int AC, int speed, int flySpeed, Status stats)
-        //{
-        //    PlayerCount++;
-
-        //    this.ID = PlayerCount - 1;
-
-        //    this.Name = name;
-        //    this.Race = race;
-        //    this.levelByClass.Add(_class, 1);
-
-        //    this.XP = 0;
-        //    this.totalLevel = 1;
-
-        //    this.AC = AC;
-        //    this.MaxHP = this.HP = HP;
-        //    this.HitDice = 1;
-        //    this.DeathSaves = (0, 0);
-
-        //    this.Stats = stats;
-
-        //    StatsMod = new Status
-        //    {
-        //        STR = getModifier(Stats.STR),
-        //        DEX = getModifier(Stats.DEX),
-        //        CON = getModifier(Stats.CON),
-        //        INT = getModifier(Stats.INT),
-        //        WIS = getModifier(Stats.WIS),
-        //        CHA = getModifier(Stats.CHA)
-        //    };
-        //}
 
         public Player(string name, string race, string _class, int HP, int AC, Speed speed, Status stats,
             bool proficiencyInsight, bool proficiencyInvestigation, bool proficiencyPerception)
@@ -107,6 +79,10 @@ namespace Deez_Notes_Dm.Models
                 CHA = getModifier(Stats.CHA)
             };
 
+            this.proficiencyInsight = proficiencyInsight;
+            this.proficiencyPerception = proficiencyPerception;
+            this.proficiencyInvestigation = proficiencyInvestigation;
+
             this.PassiveInsight = 10 + StatsMod.WIS + (proficiencyInsight ? proficiencyByLevel[1] : 0);
             this.PassiveInvestigation = 10 + StatsMod.INT + (proficiencyInvestigation ? proficiencyByLevel[1] : 0);
             this.PassivePerception = 10 + StatsMod.WIS + (proficiencyPerception ? proficiencyByLevel[1] : 0);
@@ -120,17 +96,27 @@ namespace Deez_Notes_Dm.Models
 
                 while (totalLevel < 20 && this.XP > XPbyLevel[this.totalLevel + 1])
                 {
-                    this.totalLevel++;
-                    this.HitDice++;
-
-                    if (levelByClass.Count == 1)
-                    {
-                        levelByClass[levelByClass.First().Key]++;
-                    }
+                    levelUp();
                 }
             }
 
+        }
+
+        public void levelUp()
+        {
+            this.totalLevel++;
+            this.HitDice++;
+
+            if (levelByClass.Count == 1)
+            {
+                levelByClass[levelByClass.First().Key]++;
+            }
+
             //if(nr of classes > 1) choose class (retun message?), separate method for classLevelUp
+
+            this.PassiveInsight = 10 + StatsMod.WIS + (proficiencyInsight ? proficiencyByLevel[totalLevel] : 0);
+            this.PassiveInvestigation = 10 + StatsMod.INT + (proficiencyInvestigation ? proficiencyByLevel[totalLevel] : 0);
+            this.PassivePerception = 10 + StatsMod.WIS + (proficiencyPerception ? proficiencyByLevel[totalLevel] : 0);
         }
     }
 }
