50ec8b114c39d48d5d02ff39000d5ad12863bfaf
Munteanu Teofil <andreiteofil01@gmail.com>
2023-01-19 03:29:13 +0200

    Add level system(to debug)

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace Deez_Notes_Dm.Models
 {
 
     public class Player : Creature
     {
-        static readonly int[] XPbyLevel = { -1, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000 };
+        public static readonly int[] XPbyLevel = { -1, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000, 64000, 85000, 100000, 120000, 140000, 165000, 195000, 225000, 265000, 305000, 355000 };
 
         public int xp { get; set; }
         public int XP
@@ -31,9 +30,9 @@ namespace Deez_Notes_Dm.Models
 
         private static readonly int[] proficiencyByLevel = { -1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6 };
 
-        private bool proficiencyInsight;
-        private bool proficiencyInvestigation;
-        private bool proficiencyPerception;
+        public bool ProficiencyInsight;
+        public bool ProficiencyPerception;
+        public bool ProficiencyInvestigation;
 
         public Player()
         {
@@ -50,13 +49,13 @@ namespace Deez_Notes_Dm.Models
 
             HitDice = 1;
 
-            proficiencyInsight = proficiency_insight;
-            proficiencyPerception = proficiency_perception;
-            proficiencyInvestigation = proficiency_investigation;
+            ProficiencyInsight = proficiency_insight;
+            ProficiencyPerception = proficiency_perception;
+            ProficiencyInvestigation = proficiency_investigation;
 
-            PassiveInsight = getPassiveStat(StatsMod.WIS, proficiencyInsight);
-            PassivePerception = getPassiveStat(StatsMod.WIS, proficiencyPerception);
-            PassiveInvestigation = getPassiveStat(StatsMod.INT, proficiencyInvestigation);
+            PassiveInsight = getPassiveStat(StatsMod.WIS, ProficiencyInsight);
+            PassivePerception = getPassiveStat(StatsMod.WIS, ProficiencyPerception);
+            PassiveInvestigation = getPassiveStat(StatsMod.INT, ProficiencyInvestigation);
         }
 
         //for provider
@@ -78,37 +77,12 @@ namespace Deez_Notes_Dm.Models
             PassiveInvestigation = passiveInvestigation;
         }
 
-        //public void addXP(int XP)
-        //{
-        //    if (XP > 0)
-        //    {
-        //        this.XP += XP;
-
-        //        while (totalLevel < 20 && this.XP > XPbyLevel[this.totalLevel + 1])
-        //        {
-        //            levelUp();
-        //        }
-        //    }
-        //}
-
-        public void levelUp()
+        public int getPassiveStat(int statMod, bool isProficient)
         {
-            totalLevel++;
-            HitDice++;
-
-            if (levelByClass.Count == 1)
-            {
-                levelByClass[levelByClass.First().Key]++;
-            }
-
-            //if(nr of classes > 1) choose class (retun message?), separate method for classLevelUp
-
-            PassiveInsight = getPassiveStat(StatsMod.WIS, proficiencyInsight);
-            PassiveInvestigation = getPassiveStat(StatsMod.INT, proficiencyInvestigation);
-            PassivePerception = getPassiveStat(StatsMod.WIS, proficiencyPerception);
+            return 10 + statMod + (isProficient ? proficiencyByLevel[totalLevel] : 0);
         }
 
-        public int getPassiveStat(int statMod, bool isProficient)
+        public static int getPassiveStat(int statMod, bool isProficient, int totalLevel)
         {
             return 10 + statMod + (isProficient ? proficiencyByLevel[totalLevel] : 0);
         }
--- Services/PlayerUpdateService.cs
+++ Services/PlayerUpdateService.cs
@@ -1,4 +1,7 @@
-﻿using Deez_Notes_Dm.JsonManagers;
+﻿using Deez_Notes_Dm.Json_DTOs;
+using Deez_Notes_Dm.JsonManagers;
+using Deez_Notes_Dm.Models;
+using System.Linq;
 
 namespace Deez_Notes_Dm.Services
 {
@@ -9,22 +12,74 @@ namespace Deez_Notes_Dm.Services
         {
             _playersJsonManager = PlayersJsonManager.Instance;
         }
+        private static Player ToPlayer(PlayerDTO p)
+        {
+            return new Player(p.ID, p.Name, p.Race, p.MaxHP, p.AC, p.Speeds, p.BaseStats, p.XP, p.levelByClass, p.HitDice, p.PassiveInsight, p.PassivePerception, p.PassiveInvestigation);
+        }
 
+        private PlayerDTO ToPlayerDTO(Player player)
+        {
+            return new PlayerDTO()
+            {
+                ID = player.ID,
+                Name = player.Name,
+                Race = player.Race,
+                MaxHP = player.MaxHP,
+                HP = player.HP,
+                AC = player.AC,
+                Speeds = player.Speeds,
+                BaseStats = player.BaseStats,
+                XP = player.XP,
+                levelByClass = player.levelByClass,
+                HitDice = player.HitDice,
+                PassiveInsight = player.PassiveInsight,
+                PassivePerception = player.PassivePerception,
+                PassiveInvestigation = player.PassiveInvestigation
+            };
+        }
+
+        //sometimes doesnt work
         public void AddXpToPlayer(int id, int xp)
         {
             if (xp > 0)
             {
-                _playersJsonManager.Players[id].XP += xp;
+                Player player = ToPlayer(_playersJsonManager.Players[id]);
 
-                //while (totalLevel < 20 && this.XP > XPbyLevel[this.totalLevel + 1])
-                //{
-                //    levelUp();
-                //}
+                player.XP += xp;
+
+                while (player.totalLevel < 20 && player.XP > Player.XPbyLevel[player.totalLevel + 1])
+                {
+                    LevelUpPlayer(id);
+                    player = ToPlayer(_playersJsonManager.Players[id]);
+                }
+
+                _playersJsonManager.Players[id] = ToPlayerDTO(player);
 
                 _playersJsonManager.SavePlayers();
             }
+        }
+
+        public void LevelUpPlayer(int id)
+        {
+            Player player = ToPlayer(_playersJsonManager.Players[id]);
+
+            if (player.levelByClass.Count == 1)
+            {
+                player.levelByClass[player.levelByClass.First().Key]++;
+            }
+            //else choose class to level
+
+            player.totalLevel++;
+            player.HitDice++;
+
+            player.PassiveInsight = player.getPassiveStat(player.StatsMod.WIS, player.ProficiencyInsight);
+            player.PassivePerception = player.getPassiveStat(player.StatsMod.WIS, player.ProficiencyPerception);
+            player.PassiveInvestigation = player.getPassiveStat(player.StatsMod.INT, player.ProficiencyInvestigation);
 
 
+            _playersJsonManager.Players[id] = ToPlayerDTO(player);
+
+            _playersJsonManager.SavePlayers();
         }
     }
 }
