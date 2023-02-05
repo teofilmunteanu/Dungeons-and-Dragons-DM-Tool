76f480f8a373e96e9d08d600fc7d0ac783ee5bab
Munteanu Teofil <andreiteofil01@gmail.com>
2023-02-05 11:33:49 +0200

    Update MonsterViewModel.cs

--- ViewModels/MonsterViewModel.cs
+++ ViewModels/MonsterViewModel.cs
@@ -1,41 +1,78 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Deez_Notes_Dm.Models;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using static Deez_Notes_Dm.Models.Creature;
 
 namespace Deez_Notes_Dm.ViewModels
 {
-    public class MonsterViewModel : ViewModelBase
+    public class MonsterViewModel : CreatureViewModel
     {
-        protected void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-        {
-            field = value;
-            OnPropertyChanged(propertyName);
-        }
+        public string Size { get; set; }
+        public string Alignment { get; set; }
 
-        private string damageVulnerabilities;
-        public string DamageVulnerabilities
-        {
-            get => damageVulnerabilities;
-            set => SetField(ref damageVulnerabilities, value);
-        }
 
-        private string damageResistances;
-        public string DamageResistances
-        {
-            get => damageResistances;
-            set => SetField(ref damageResistances, value);
-        }
+        public string Hit_Dice { get; set; }
 
-        private string damageImmunities;
-        public string DamageImmunities
-        {
-            get => damageImmunities;
-            set => SetField(ref damageImmunities, value);
-        }
+        public Stats SavingThrows { get; set; }
+        //if null, take deafult(calculate)
+        public string Skills { get; set; }
 
-        private string conditionImmuniuties;
-        public string ConditionImmuniuties
+
+        public string DamageVulnerabilities { get; set; }
+
+        public string DamageResistances { get; set; }
+
+        public string DamageImmunities { get; set; }
+
+        public string ConditionImmuniuties { get; set; }
+
+        public string Senses { get; set; }
+
+        public string Languages { get; set; }
+
+
+        public double ChallengeRating { get; set; }
+        public int XP_Drop { get; set; } // generate using challangeRating
+
+
+        public string[] Actions { get; set; } //make class?
+        public string[] Reactions { get; set; }
+
+
+        public string LegendaryActionDescription { get; set; }
+        public string[] LegendaryActions { get; set; }
+
+        public string[] SpecialAbilities { get; set; }
+
+
+        public ICommand AddHPCommand { get; }
+        public ICommand SubtractHPCommand { get; }
+
+        public MonsterViewModel(Monster monster): base(monster.ID, monster.Name, monster.Race, monster.HP, monster.MaxHP, monster.AC,
+            monster.Speeds, monster.BaseStats, monster.StatsMod)
         {
-            get => conditionImmuniuties;
-            set => SetField(ref conditionImmuniuties, value);
+            Size = monster.Size;
+            Alignment = monster.Alignment;
+            Hit_Dice = monster.Hit_Dice;
+            SavingThrows = monster.SavingThrows;
+            Skills = monster.Skills;
+            DamageVulnerabilities = monster.DamageVulnerabilities;
+            DamageResistances = monster.DamageResistances;
+            DamageImmunities = monster.DamageImmunities;
+            ConditionImmuniuties = ConditionImmuniuties;
+            Senses = monster.Senses;
+            Languages = monster.Languages;
+            ChallengeRating = monster.ChallengeRating;
+            XP_Drop = monster.XP_Drop;
+            Actions = monster.Actions;
+            Reactions = monster.Reactions;
+            LegendaryActionDescription = monster.LegendaryActionDescription;
+            LegendaryActions = monster.LegendaryActions;
+            SpecialAbilities = monster.SpecialAbilities;
+
+            //creatureManager????
+            //AddHPCommand = new AddHPCommand(this, playerListViewModel, playersManager); 
+            //SubtractHPCommand = new SubtractHPCommand(this, playerListViewModel, playersManager); 
         }
     }
 }
